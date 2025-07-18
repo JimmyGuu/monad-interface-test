b18baaf486f35e184030dd05d370a67d5f9d7562
jimmygu <jimmygujh@gmail.com>
2025-07-18 11:53:00 +0800

    fix: remove custom connectors

--- src/context/wagmi.tsx
+++ src/context/wagmi.tsx
@@ -3,21 +3,9 @@
 import { config, metadata, projectId } from "@/configs/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { type ReactNode } from "react";
-import { cookieToInitialState, WagmiProvider, createConfig, fallback, http } from "wagmi";
+import { cookieToInitialState, WagmiProvider } from "wagmi";
 import { DEFAULT_CHAIN_ID } from '@/configs';
-import { RainbowKitProvider, connectorsForWallets, darkTheme } from "@rainbow-me/rainbowkit";
-import {
-  metaMaskWallet,
-  coinbaseWallet,
-  okxWallet,
-  bitgetWallet,
-  binanceWallet,
-  injectedWallet,
-  walletConnectWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { createClient } from "viem";
-import { mainnet, monadTestnet } from "viem/chains";
-
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
@@ -25,50 +13,6 @@ const queryClient = new QueryClient();
 if (!projectId) {
   throw new Error("Project ID is not defined");
 }
-
-const connectors: any = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended',
-      wallets: [
-        metaMaskWallet,
-        coinbaseWallet,
-        okxWallet,
-        bitgetWallet,
-        binanceWallet,
-        injectedWallet,
-        walletConnectWallet,
-      ],
-    },
-  ],
-  {
-    appName: metadata.name,
-    projectId,
-  }
-);
-
-const wagmiConfig = createConfig({
-  ...config,
-  connectors,
-  client: ({ chain }) => {
-    if (chain.id === monadTestnet.id) {
-      return createClient({
-        chain,
-        transport: http("https://testnet-rpc.monad.xyz"),
-      });
-    }
-    if (chain.id === mainnet.id) {
-      return createClient({
-        chain,
-        transport: http("https://eth.merkle.io"),
-      });
-    }
-    return createClient({
-      chain,
-      transport: http()
-    });
-  }
-});
 
 function ContextProvider({
   children,
@@ -78,13 +22,13 @@ function ContextProvider({
   cookies?: string | null;
 }) {
   const initialState = cookieToInitialState(
-    wagmiConfig,
+    config,
     cookies
   );
 
   return (
     <WagmiProvider
-      config={wagmiConfig}
+      config={config}
       initialState={initialState}
     >
       <QueryClientProvider client={queryClient}>
